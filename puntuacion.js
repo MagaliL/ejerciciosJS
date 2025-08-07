bef3b9b3eda3bba12ff7bb649340b58b7c03adfb
Magali L. Espinosa <magalizethe@gmail.com>
2025-08-07 16:55:21 -0600

    Se está trabajando en el juego de la puntuación

--- puntuacion.js
+++ puntuacion.js
@@ -1,20 +1,25 @@
 //Variables
 let numeroSecreto= 6;
-let numeroUsuario = prompt("Me indicas un número entre 1 y 10, por favor: ");
+let numeroUsuario=0;
 
-// Una buena práctica es poner las variables, la descripción junta
 
-console.log(numeroUsuario);
+while(numeroUsuario != numeroSecreto){
+    let numeroUsuario = prompt("Me indicas un número entre 1 y 10, por favor: ");
 
-/*Este bloque de código es una condición para comparar los números que dio el usuario y el número secreto que ya se encuentra almacenado en la variable numeroSecreto*/
+    // Una buena práctica es poner las variables, la descripción junta
 
-if (numeroUsuario == numeroSecreto ) {
-    alert(`Acertaste, el número secreto es: ${numeroSecreto}`)
-    //Se cumple la condición
+    console.log(numeroUsuario);
 
-}else {
-    alert("Lo sentimos, tu número no es correcto.")
-    //No se cumple la condición
+    /*Este bloque de código es una condición para comparar los números que dio el usuario y el número secreto que ya se encuentra almacenado en la variable numeroSecreto*/
+
+    if (numeroUsuario == numeroSecreto ) {
+        alert(`Acertaste, el número secreto es: ${numeroSecreto}`)
+        //Se cumple la condición
+
+    }else {
+        alert("Lo sentimos, tu número no es correcto.")
+        //No se cumple la condición
+    }
+
+
 }
-
-
